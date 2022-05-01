3004753d341ddff7c8e9909c1d7d676b2cb41837
Pablo Kurskii <pablo.kurskii@gmail.com>
2022-05-01 15:36:56 +0200

    Update ThreadCommunication.scala
    More producers and consumers
    check out the difference between old algo and new

--- src/main/scala/lectures/part3concurrency/ThreadCommunication.scala
+++ src/main/scala/lectures/part3concurrency/ThreadCommunication.scala
@@ -14,7 +14,9 @@ object ThreadCommunication extends App {
     private var value: Int = 0
 
     def isEmpty: Boolean = value == 0
+
     def set(newValue: Int) = value = newValue
+
     def get = {
       val result = value
       value = 0
@@ -27,11 +29,11 @@ object ThreadCommunication extends App {
 
     val consumer = new Thread(() => {
       println("[consumer] waiting...")
-      while(container.isEmpty) {
+      while (container.isEmpty) {
         println("[consumer] actively waiting...")
       }
 
-      println("[consumer] I have consumed " +  container.get)
+      println("[consumer] I have consumed " + container.get)
     })
 
     val producer = new Thread(() => {
@@ -93,7 +95,7 @@ object ThreadCommunication extends App {
     val consumer = new Thread(() => {
       val random = new Random()
 
-      while(true) {
+      while (true) {
         buffer.synchronized {
           if (buffer.isEmpty) {
             println("[consumer] buffer empty, waiting...")
@@ -116,7 +118,7 @@ object ThreadCommunication extends App {
       val random = new Random()
       var i = 0
 
-      while(true) {
+      while (true) {
         buffer.synchronized {
           if (buffer.size == capacity) {
             println("[producer] buffer is full, waiting...")
@@ -141,5 +143,88 @@ object ThreadCommunication extends App {
     producer.start()
   }
 
-   prodConsLargeBuffer()
+  // prodConsLargeBuffer()
+
+  /*
+    Prod-cons, level 3
+         producer1 ->  [ ? ? ? ] -> consumer1
+         producer2 -----^     ^---- consumer2
+   */
+
+
+  class Consumer(id: Int, buffer: mutable.Queue[Int]) extends Thread {
+    override def run(): Unit = {
+      val random = new Random()
+
+      while (true) {
+        buffer.synchronized {
+          /*
+            producer produces value, two Cons are waiting
+            notifies ONE consumer, notifies on buffer
+            notifies the other consumer
+           */
+          /*IMPORTANT*/
+          while (buffer.isEmpty) {
+            println(s"[consumer $id] buffer empty, waiting...")
+            buffer.wait()
+          }
+
+          // there must be at least ONE value in the buffer
+          val x = buffer.dequeue() // OOps.!
+          println(s"[consumer $id] consumed " + x)
+
+          buffer.notifyAll()
+        }
+
+        Thread.sleep(random.nextInt(250))
+      }
+    }
+  }
+
+  class Producer(id: Int, buffer: mutable.Queue[Int], capacity: Int) extends Thread {
+    override def run(): Unit = {
+      val random = new Random()
+      var i = 0
+
+      while (true) {
+        buffer.synchronized {
+          /*IMPORTANT*/
+          while (buffer.size == capacity) {
+            println(s"[producer $id] buffer is full, waiting...")
+            buffer.wait()
+          }
+
+          // there must be at least ONE EMPTY SPACE in the buffer
+          println(s"[producer $id] producing " + i)
+          buffer.enqueue(i)
+
+          /*because of this notifyAll, even though consumer awakn, it checks if buffer is ont empty before proceed*/
+          buffer.notifyAll()
+
+          i += 1
+        }
+
+        Thread.sleep(random.nextInt(500))
+      }
+    }
+  }
+
+  def multiProdCons(nConsumers: Int, nProducers: Int): Unit = {
+    val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
+    val capacity = 20
+
+    (1 to nConsumers).foreach(i => new Consumer(i, buffer).start())
+    (1 to nProducers).foreach(i => new Producer(i, buffer, capacity).start())
+  }
+
+  // multiProdCons(3, 6)
+
+  /*
+    Exercises.
+    1) think of an example where notifyALL acts in a different way than notify?
+    2) create a deadlock
+    3) create a livelock
+   */
+
+  
 }
